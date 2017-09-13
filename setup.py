9de26b1990664cda8b93a08a7a2653b23a7b93aa
Romain Martinez <martinez.staps@gmail.com>
2017-09-13 17:04:00 -0400

    add requirement (click==6.7)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'Click>=6.0',
+    'Click==6.7',
     # TODO: put package requirements here
 ]
 
