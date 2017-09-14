98828874d31837f5bbf8ff0a59a938f437ba94df
Romain Martinez <martinez.staps@gmail.com>
2017-09-14 17:53:05 -0400

    main menu

--- bmch/cli.py
+++ bmch/cli.py
@@ -4,10 +4,24 @@
 
 import cursesmenu as cm
 
-# categories
-main_cat = ('preprocessing', 'processing', 'statistics', 'plot')
-sub_cat = {
-    'preprocessing': ('')
-}
-# main menu
-menu = cm.CursesMenu(title='main title', subtitle='subtitle')
+
+class Menu:
+    """the menu class."""
+
+    def __init__(self):
+        self.main_cat = ['preprocessing', 'processing', 'statistics', 'plot']
+        self.sub_cat = {
+            'preprocessing': [
+                'create new project', 'import project', 'import files', 'model construction', 'kinematic reconstruction'
+            ]
+        }
+
+    def mainmenu(self):
+        """Getting a selection out of a list of strings."""
+        selection = cm.SelectionMenu.get_selection(self.main_cat)
+        return selection
+
+
+if __name__ == '__main__':
+    menu = Menu()
+    selection = menu.mainmenu()
