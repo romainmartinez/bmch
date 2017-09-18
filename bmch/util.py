802db704beb0bfc4ba33bc8c6a6cd917cadf0573
Romain Martinez <martinez.staps@gmail.com>
2017-09-18 12:04:47 -0400

    conf files headers

--- bmch/util.py
+++ bmch/util.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+
+
+def metaheader(case, path=None):
+    # TODO: doc
+    import csv
+    headers = {
+        'emg': ['muscle_id', 'publication_name'],
+        'markers': ['marker_id'],
+        'force': ['analog_id'],
+        'participants': ['pseudo', 'process', 'laterality', 'group', 'mass', 'height', 'date'],
+        'trials': ['folder', 'emg', 'markers', 'force']
+    }
+    # TODO: add headers
+    with open(path + case + '.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers[case])
+        writer.writeheader()
