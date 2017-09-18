0605e98aa04298aa9094d5fb5d097564c7fc1f37
Romain Martinez <martinez.staps@gmail.com>
2017-09-18 04:57:47 -0400

    launch submodules from cli

--- bmch/cli.py
+++ bmch/cli.py
@@ -4,24 +4,45 @@
 
 import menu
 
+import bmch
 
-# def coucou():
-#     print('coucou')
-#
-# header = '-' * 21
-#
-# main = menu.Menu(title=header, message='root')
-# main.set_options([("new option name", coucou),
-#                   ('exit', menu.Menu.CLOSE)])
-#
-# main.open()
-#
-#
-# main.close()
 
-main = menu.Menu(title="Main Menu")
-sub = menu.Menu(title="Submenu")
-main.set_options([("Open submenu", sub.open),
-                  ("Close main menu", main.close)])
-sub.set_options([("Return to main menu", sub.close)])
-main.open()
+def show_menu():
+    """Display command line interface (without arguments and outputs)."""
+
+    # create main menu
+    main = menu.Menu(title="Main Menu")
+
+    # create submenu
+
+    # 1) preprocessing submenu
+    preproc = menu.Menu(title="preprocessing")
+    preproc.set_options([('create project', bmch.preprocessing.createproject),
+                         ('import project', bmch.preprocessing.importproject),
+                         ('import files', bmch.preprocessing.importfiles),
+                         ('return', preproc.close)])
+
+    # 2) processing submenu
+    proc = menu.Menu(title='processing')
+    proc.set_options([('return', proc.close)])
+
+    # 3) statistics submenu
+    stats = menu.Menu(title='statistics')
+    stats.set_options([('return', stats.close)])
+
+    # 4) plot submenu
+    plt = menu.Menu(title='plot')
+    plt.set_options([('return', plt.close)])
+
+    # add submenus
+    main.set_options([('preprocessing', preproc.open),
+                      ('processing', proc.open),
+                      ('statistics', stats.open),
+                      ('plot', plt.open),
+                      ('exit', main.close)])
+
+    main.open()
+
+
+if __name__ == '__main__':
+    show_menu()
